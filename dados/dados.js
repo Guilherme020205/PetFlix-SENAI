f3006f10f6c82435306ea78e4dc3b209d0c4f87f
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-08-05 21:54:53 -0300

    Exibindo snopse

--- dados/dados.js
+++ dados/dados.js
@@ -17,7 +17,7 @@ const filme = [
         tempo: "1 hora e 40 minutos",
         data_lancamento: "2005",
         descricao: "A vida é difícil para o filho do mineiro de Yorkshire, Joe Carraclough (Jonathan Mason), que é espancado na escola por seu professor, seu único consolo é seu collie Lassie. Fica pior: quando a mina é desativada, seu pai, Sam (John Lynch), é forçado a vender o cachorro para o Duque (Peter O Toole), que é dono da propriedade local. O servo do Duque, Hynes (Steve Pemberton), assusta o cachorro, que continua correndo de volta, então os Carracloughs têm que continuar devolvendo-a, até que o Duque se mude para as Terras Altas da Escócia para a temporada de férias. Lassie escapa, embarcando em uma jornada desesperada para casa, com assustadores apanhadores de cães de Glasgow e acolhida por um artista de circo. Parece que um milagre é necessário, até o Natal.",
-        capa: "./img/Lassie.jpg",
+        capa: "https://agenciadenoticias.uniceub.br/wp-content/uploads/2024/04/garfield-movie-ver24-xlg-button-1712851392211.jpg",  
         genero: 1
     },
     {
@@ -26,7 +26,10 @@ const filme = [
         tempo: "1 h 41 min",
         data_lancamento: "2024",
         descricao: "Garfield (dublado por Chris Pratt), o gato doméstico mundialmente famoso, que odeia segundas-feiras e adora lasanha, está prestes a ter uma aventura selvagem ao ar livre. Após um reencontro inesperado com seu pai há muito perdido - o gato de rua desleixado Vic (dublado por Samuel L. Jackson) - Garfield e seu amigo canino Odie são forçados a deixar sua vida perfeitamente mimada para se juntar a Vic em um assalto hilário e arriscado.", 
-        capa: "./img/Garfield.jfif",
+        capa: "https://agenciadenoticias.uniceub.br/wp-content/uploads/2024/04/garfield-movie-ver24-xlg-button-1712851392211.jpg",
         genero: 2
     },
 ]
+
+
+// const filho = ["Filho0", "Filho2","Filho2"] 
